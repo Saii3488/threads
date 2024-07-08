08807be7ee0dadf3126b3d3d96541935ee71c944
Saii3488 <saiipranav197@gmail.com>
2024-07-08 19:33:45 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import  dotenv from "dotenv";
+import connectDB from "./db/Connectdb.js";
 dotenv.config();
+connectDB();
 const app=express();
 const PORT=process.env.PORT||5000
 app.listen(5000,()=> console.log(`server started at ${PORT} hey`))
