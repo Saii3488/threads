2f33d2571de219f22b7c89a63ab94d9ec0eda0f6
Saii3488 <saiipranav197@gmail.com>
2024-07-11 14:58:20 +0530

    feedposts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -95,4 +95,52 @@ const likeUnlikePost=async(req,res)=>{
 		console.log(err);
 	}
 }
-export{createPost,getPost,deletePost,likeUnlikePost}
+const replytoPost=async(req,res)=>{
+	try{
+		const { text } = req.body;
+		const postId = req.params.id;
+		const userId = req.user._id;
+		const userProfilePic = req.user.profilePic;
+		const username = req.user.username;
+
+		if (!text) {
+			return res.status(400).json({ error: "Text field is required" });
+		}
+
+		const post = await Post.findById(postId);
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		const reply = { userId, text, userProfilePic, username };
+
+		post.replies.push(reply);
+		await post.save();
+
+		res.status(200).json(reply);
+
+	}catch(err){
+		res.status(500).json({ error: err.message });
+		console.log(err);
+	}
+}
+const getFeedPosts=async(req,res)=>{
+	try{
+		const userId=req.user._id
+		const user = await User.findById(userId);
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		const following = user.following;
+
+		const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: -1 });
+
+		res.status(200).json(feedPosts);
+
+	}catch(err){
+		res.status(500).json({ error: err.message });
+		console.log(err);
+	}
+}
+export{createPost,getPost,deletePost,likeUnlikePost,replytoPost,getFeedPosts}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,12 +1,16 @@
 import express from "express"
-import { createPost,getPost,deletePost,likeUnlikePost } from "../controllers/postController.js";
+import { createPost,getPost,deletePost,likeUnlikePost,replytoPost,getFeedPosts } from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 const router=express.Router();
+router.get("/feed",protectRoute,getFeedPosts)
+
 router.get("/:id",protectRoute,getPost)
 
 router.post("/create",protectRoute,createPost)
 router.delete("/:id",protectRoute,deletePost)
 router.post("/likes/:id",protectRoute,likeUnlikePost)
+router.post("/reply/:id",protectRoute,replytoPost)
+
 
 
 export default router;
