d66949cfb965ec7b288aa695398dfc35ba1ac1a0
Saii3488 <saiipranav197@gmail.com>
2024-07-11 10:05:40 +0530

    likeunlikepost

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { createPost,getPost,deletePost } from "../controllers/postController.js";
+import { createPost,getPost,deletePost,likeUnlikePost } from "../controllers/postController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 const router=express.Router();
 router.get("/:id",protectRoute,getPost)
 
 router.post("/create",protectRoute,createPost)
 router.delete("/:id",protectRoute,deletePost)
+router.post("/likes/:id",protectRoute,likeUnlikePost)
+
 
 export default router;
